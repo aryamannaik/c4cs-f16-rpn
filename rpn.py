754baea06c3224bf821bc645fc43f1604f177609
Aryaman Naik <arya@0587391211.wireless.umich.net>
2016-10-14 13:24:30 -0400

    Added colours

--- rpn.py
+++ rpn.py
@@ -1,9 +1,20 @@
 #!/usr/bin/env python3
 
 import operator
+from termcolor import colored
 
 def concat(arg1, arg2):
     return int(str(arg1) + str(arg2))
+
+def col_print(stk):
+    print("[ ", end="")
+    for ele in stk:
+        if ele in operators:
+           print(colored(ele, "green"), end=" ]\n")
+        elif int(ele) < 0:
+            print(colored(ele, "red"), end=", ")
+        else:
+            print(ele, end=", ")
 
 operators = {
     '+' : operator.add,
@@ -16,17 +27,17 @@ operators = {
 
 def calculate(arg):
     stack = list()
+    col_print(arg.split())
     for token in arg.split():
         try:
             token = int(token)
             stack.append(token)
-        except ValueError:
+        except:
             fn = operators[token]
             arg2 = stack.pop()
             arg1 = stack.pop()
             result = fn(arg1, arg2)
             stack.append(result)
-        print(stack)
 
     if len(stack) != 1:
         raise TypeError("Too many parameters")
